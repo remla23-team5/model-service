d02b26fc259aedbb16c3f3f37f1f0b08b9fe3e86
Luca Cras <luca_cras@hotmail.com>
2023-05-08 09:11:56 +0200

    fix linting error

--- test/test_text_preprocessing.py
+++ test/test_text_preprocessing.py
@@ -1,6 +1,6 @@
-import pytest
+from unittest import TestCase
 
-class TestTextPreprocessing:
+class TestTextPreprocessing(TestCase):
     def test_clean_text(self):
         # TODO: Write a test case for clean_text
         pass
